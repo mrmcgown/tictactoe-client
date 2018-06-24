3672af49c227707eb08347f8fbceee5693964108
mrmcgown <mrmcgown@gmail.com>
2018-06-23 21:51:58 -0400

    Index.html, Curl, and Auth scripts
    Functionality added for sign up, sign in, change password, and sign out.

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const getFormFields = require('../../../lib/get-form-fields')
+
+const api = require('./api')
+const ui = require('./ui')
+
+const onSignUp = function (event) {
+  event.preventDefault()
+  console.log('sign up ran!')
+
+  const data = getFormFields(this)
+  console.log(this)
+  console.log(data)
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
+}
+
+const onSignIn = function (event) {
+  event.preventDefault()
+  console.log('sign in ran!')
+
+  const data = getFormFields(this)
+  console.log(this)
+  console.log(data)
+  api.signIn(data)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+
+const onChangePassword = function (event) {
+  event.preventDefault()
+  console.log('change password ran')
+
+  const data = getFormFields(this)
+
+  api.changePassword(data)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
+}
+
+const onSignOut = function (event) {
+  event.preventDefault()
+  console.log('signout ran')
+  api.signOut()
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+}
+
+const addHandlers = () => {
+  $('#sign-up').on('submit', onSignUp)
+  $('#sign-in').on('submit', onSignIn)
+  $('#change-password').on('submit', onChangePassword)
+  $('#sign-out').on('submit', onSignOut)
+}
+
+module.exports = {
+  addHandlers
+}
---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const store = require('../store')
+
+const signUpSuccess = function (data) {
+  $('#message').text('Signed up successfully')
+  $('#message').css('background-color', 'green')
+  console.log('signUpSuccess ran. Data is :', data)
+}
+
+const signUpFailure = function (error) {
+  $('#message').text('Error on sign up')
+  $('#message').css('background-color', 'red')
+  console.log('signUpFailure ran. Error is :', error)
+}
+
+const signInSuccess = function (data) {
+  $('#message').text('Signed in successfully')
+  $('#message').css('background-color', 'green')
+  console.log('signInSuccess ran. Data is :', data)
+  store.user = data.user
+}
+
+const signInFailure = function (error) {
+  $('#message').text('Error on sign in')
+  $('#message').css('background-color', 'red')
+  console.log('signInFailure ran. Error is :', error)
+}
+
+const changePasswordSuccess = function (data) {
+  $('#message').text('Changed password successfully')
+  $('#message').css('background-color', 'green')
+  console.log('changePasswordSuccess ran. Data is :', data)
+}
+
+const changePasswordFailure = function (error) {
+  $('#message').text('Error on change password')
+  $('#message').css('background-color', 'red')
+  console.log('changePasswordFailure ran. Error is :', error)
+}
+
+const signOutSuccess = function () {
+  $('#message').text('Signed out successfully')
+  $('#message').css('background-color', 'green')
+  console.log('signOutSuccess ran. Data is :')
+  store.user = null
+}
+
+const signOutFailure = function (error) {
+  $('#message').text('Error on sign out')
+  $('#message').css('background-color', 'red')
+  console.log('signOutFailure ran. Error is :', error)
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutFailure,
+  signOutSuccess
+}
